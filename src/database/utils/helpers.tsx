fa129df9a1c7cae5751d3e9512a657f935e21d64
Patrick Loser <p_loser_Austria@gmx.at>
2024-11-18 23:20:00 +0100

    NovelQueries

--- src/database/utils/helpers.tsx
+++ src/database/utils/helpers.tsx
@@ -1,12 +1,35 @@
-import {SQLiteBindParams, SQLiteDatabase} from 'expo-sqlite';
+import {SQLiteBindParams, SQLiteDatabase, SQLiteRunResult} from 'expo-sqlite';
+import {noop} from 'lodash-es';
 
-export function runTransaction(
+export type QueryObject = Array<
+  | [string]
+  | [string, SQLiteBindParams | undefined]
+  | [
+      string,
+      SQLiteBindParams | undefined,
+      ((data: SQLiteRunResult) => void) | undefined,
+    ]
+  | [
+      string,
+      SQLiteBindParams | undefined,
+      ((data: SQLiteRunResult) => void) | undefined,
+      ((data: any) => void) | undefined,
+    ]
+>;
+export async function runTransaction(
   db: SQLiteDatabase,
-  queryObject: Array<[string] | [string, SQLiteBindParams | undefined]>,
+  queryObject: QueryObject,
 ) {
   db.withTransactionAsync(async () => {
-    for (const [query, params] of queryObject) {
-      db.runAsync(query, params ?? []);
+    for (const [
+      query,
+      params,
+      callback = noop,
+      catchCallback = noop,
+    ] of queryObject) {
+      db.runAsync(query, params ?? [])
+        .then(callback)
+        .catch(catchCallback);
     }
   });
 }
@@ -19,6 +42,21 @@ export function getAllTransaction(
     db.withTransactionAsync(async () => {
       for (const [query, params] of queryObject) {
         db.getAllAsync(query, params ?? []).then(res => {
+          resolve(res as any);
+        });
+      }
+    }),
+  );
+}
+
+export function getFirstTransaction(
+  db: SQLiteDatabase,
+  queryObject: Array<[string] | [string, SQLiteBindParams | undefined]>,
+) {
+  return new Promise(resolve =>
+    db.withTransactionAsync(async () => {
+      for (const [query, params] of queryObject) {
+        db.getFirstAsync(query, params ?? []).then(res => {
           resolve(res as any);
         });
       }
